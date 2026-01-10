bd16accb1e1a01f5941cfccf5c100964600b44dc
AlanGustav0 <alagustavo946@gmail.com>
2026-01-10 10:27:50 -0300

    Instalação de pacotes

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import { AuthProvider } from './infra/context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App/>);
+root.render(
+<AuthProvider><App/></AuthProvider>
+
+);
